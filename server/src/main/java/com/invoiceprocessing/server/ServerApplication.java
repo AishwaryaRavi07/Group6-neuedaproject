991e70be831093b52487b36b95e9f32e1f862ac5
Cynthia <cynthia.konar@intellection.com>
2024-06-23 00:00:01 +0530

    feature: swagger ui added

--- server/src/main/java/com/invoiceprocessing/server/ServerApplication.java
+++ server/src/main/java/com/invoiceprocessing/server/ServerApplication.java
@@ -2,6 +2,12 @@ package com.invoiceprocessing.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @SpringBootApplication
 public class ServerApplication {
@@ -9,5 +15,4 @@ public class ServerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
 	}
-
 }
---
+++ server/src/main/java/com/invoiceprocessing/server/SwaggerConfiguration.java
@@ -0,0 +1,20 @@
+package com.invoiceprocessing.server;
+
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfiguration {
+
+    @Bean
+    public OpenAPI apiDocConfig() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Invoice & Inventory Management API")
+                        .description("Welcome to the Invoice & Inventory Management API. Manage invoices, track inventory, and retrieving detailed reports. Simplify your business operations with our comprehensive API.")
+                        .version("0.0.1"));
+    }
+}
