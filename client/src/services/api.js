86b78b69a570a19048ab2683c4fcb60fe50a35e5
Cynthia <cynthia.konar@intellection.com>
2024-06-27 15:33:15 +0530

    CORS Token issues resolved

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -4,6 +4,7 @@ const API_URL = 'http://localhost:8080';
 
 export const getAllInvoices = async () => {
     try {
+        console.log("Getting invoices");
         return await axios.get(`${API_URL}/invoice`, {
             // Token is hardcoded from /auth/login API
             headers: {
@@ -20,7 +21,13 @@ export const getAllInvoices = async () => {
 
 export const saveInvoice = async (payload) => {
     try {
-        return await axios.post(`${API_URL}/invoice`, payload);
+        return await axios.post(`${API_URL}/invoice`, payload, {
+            headers: {
+                "Authorization": `Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhZG1pbiIsImlhdCI6MTcxOTQ3MzU1NywiZXhwIjoxNzE5NTU5OTU3fQ.j4_fj4Nks_iW-z8RUfOOhTENsUFgHAWmrn-oTt48Zv0`,
+                "Content-Type": 'application/json',
+                "Access-Control-Allow-Origin": "*"
+            }
+        });
     } catch (error) {
         console.log('Error: ', error.message);
         return error.response.data;
@@ -29,7 +36,13 @@ export const saveInvoice = async (payload) => {
 
 export const deleteInvoice = async (id) => {
     try {
-        return await axios.delete(`${API_URL}/invoice/${id}`);
+        return await axios.delete(`${API_URL}/invoice/${id}`,{
+            headers: {
+                "Authorization": `Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhZG1pbiIsImlhdCI6MTcxOTQ3MzU1NywiZXhwIjoxNzE5NTU5OTU3fQ.j4_fj4Nks_iW-z8RUfOOhTENsUFgHAWmrn-oTt48Zv0`,
+                "Content-Type": 'application/json',
+                "Access-Control-Allow-Origin": "*"
+            }
+        });
     } catch (error) {
         console.log('Error: ', error.message);
         return error.response.data;
