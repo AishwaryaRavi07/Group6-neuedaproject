890bad68452cc043452fba3fff8b9133e63b9975
Cynthia <cynthia.konar@intellection.com>
2024-06-27 14:00:16 +0530

    fix

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -4,7 +4,14 @@ const API_URL = 'http://localhost:8080';
 
 export const getAllInvoices = async () => {
     try {
-        return await axios.get(`${API_URL}/invoice`);
+        return await axios.get(`${API_URL}/invoice`, {
+            // Token is hardcoded from /auth/login API
+            headers: {
+                "Authorization": `Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhZG1pbiIsImlhdCI6MTcxOTQ3MzU1NywiZXhwIjoxNzE5NTU5OTU3fQ.j4_fj4Nks_iW-z8RUfOOhTENsUFgHAWmrn-oTt48Zv0`,
+                "Content-Type": 'application/json',
+                "Access-Control-Allow-Origin": "*"
+            }
+        });
     } catch (error) {
         console.log('Error: ', error.message);
         return error.response.data;
