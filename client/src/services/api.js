1772afd6f9839ce57cacbd90df5636493f24592a
Aishwarya <aish123.ravi@gmail.com>
2024-07-07 16:08:55 +0530

    Fixed Filter Logic

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -4,19 +4,15 @@ const API_URL = 'http://localhost:8080';
 
 const getToken = () => localStorage.getItem("authToken");
 
-export const getAllInvoices = async (payload) => {
+export const getAllInvoices = async (userId = '0') => {
     try {
         const token = getToken();
         console.log(token)
-        return await axios.get(`${API_URL}/get-invoices`,{
-            userId: "2"
-          }
-         ,{
+        return await axios.get(`${API_URL}/get-invoices/${userId}`,{
             headers: {
                 'Authorization': `Bearer ${token}`,
-                'Content-Type': 'application/json',
-                'Access-Control-Allow-Origin': '*'
-            },
+                'Content-Type': 'application/json',   
+            },         
             
         });
     } catch (error) {
@@ -44,11 +40,11 @@ export const saveInvoice = async (payload) => {
 export const deleteInvoice = async (id) => {
     try {
         const token = getToken();
-        return await axios.delete(`${API_URL}/invoice/${id}`, {
+        return await axios.delete(`${API_URL}/delete-invoice`, {
             headers: {
                 'Authorization': `Bearer ${token}`,
                 'Content-Type': 'application/json',
-                'Access-Control-Allow-Origin': '*'
+                
             }
         });
     } catch (error) {
@@ -57,21 +53,21 @@ export const deleteInvoice = async (id) => {
     }
 }
 
-export const getTransaction = async () => {
+export const getTransaction = async (userId = '0') => {
     try {
         const token = getToken();
         console.log(token)
         
-        return await axios.get(`${API_URL}/get-transactions`,{
+        const response = await axios.get(`${API_URL}/get-transactions/${userId}`,{
             headers: {
                 'Authorization': `Bearer ${token}`,
                 'Content-Type': 'application/json',
                 'Access-Control-Allow-Origin': '*'
             },
-            body: {
-                userId: "0"
-              }
+            
         });
+        console.log('Response:', response.data.income);
+
     } catch (error) {
         console.log('Error: ', error.message);
         return error.response ? error.response.data : { error: error.message };
